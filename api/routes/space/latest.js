bfcdc13245c79cffed499d88e1384a67f4bac7d1
mounta90 <66538018+mounta90@users.noreply.github.com>
2024-07-28 15:50:32 -0600

    change : latest API route simplified

--- api/routes/space/latest.js
+++ api/routes/space/latest.js
@@ -1,16 +1,15 @@
 const { Router } = require("express");
 const { getLatest } = require("../../get_routes/get_latest");
-const { returnOrErrorFlag } = require("../../utils/return_or_error_flag");
 const router = Router();
 
 // Get the latest data in the past 24 hours
 function latestRouter(cache, cacheTTL) {
   router.post("/", async (req, res) => {
-    const { assetName, isHistoricalIncluded, isMeasurementPrediction } = req.body;
+    const { assetName, commodityName } = req.body;
 
     let data = [];
 
-    if (!assetName) {
+    if (!assetName || !commodityName) {
       console.log("*** Missing Data (/latest) ***");
       return res.json({
         data,
@@ -19,24 +18,8 @@ function latestRouter(cache, cacheTTL) {
       });
     }
 
-    let historicalFlag;
-    let predictionFlag;
-
     try {
-      historicalFlag = returnOrErrorFlag(isHistoricalIncluded);
-      predictionFlag = returnOrErrorFlag(isMeasurementPrediction);
-    } catch (error) {
-      console.log(error.message);
-
-      return res.json({
-        data,
-        status: "bad",
-        message: error.message,
-      });
-    }
-
-    try {
-      data = await getLatest(assetName, historicalFlag, predictionFlag);
+      data = await getLatest(assetName, commodityName);
     } catch (error) {
       console.log(error);
 
